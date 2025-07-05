459ce0c5d32f2fbe7a04a2eeda408f0cf3ba8b86
cygday <amethystpun98@gmail.com>
2025-07-05 16:49:45 +0545

    commit

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
+
+import os
+import requests
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#list of documentary yotuube video ids
+from dotenv import load_dotenv
+load_dotenv()
 
-video_ids = [
-        "T7mTuUhhiss",
-        "x9miEIklmYw",
-        "0an7kovsPeQ",
-        ]
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+
+def search_youtube(query):
+    url = "https://www.googleapis.com/youtube/v3/search"
+    params = {
+    "part": "snippet",
+    "q": query,
+    "type": "video",
+    "maxResults": 5,
+    "key": YOUTUBE_API_KEY
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    video_ids = [item["id"]["videoId"] for item in data.get("items", [])]
+    return video_ids
 
 @app.route('/')
 def index():
-    return render_template("index.html", videos=video_ids)
+    videos = search_youtube("documentary")
+    return render_template("index.html", videos=videos)
 
 if __name__ == '__main__':
     app.run(debug=True)
